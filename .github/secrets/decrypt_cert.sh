5fb8624677a87980432b8ae3c26cbeee0413e448
Javier <jgleal@gmail.com>
2020-04-03 10:22:45 +0200

    remove provisioning

--- .github/secrets/decrypt_cert.sh
+++ .github/secrets/decrypt_cert.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_CERTIFICATE_ENCRYPT" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_CERTIFICATE_ENCRYPT" --output ./.github/secrets/Certificate.p12 ./.github/secrets/Certificate.p12.gpg
-
-mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-
-cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/5ffa0f6b-a14e-40dc-a6e5-d182edbc6fe5.mobileprovision
 
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/Certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_CERTIFICATE_PASS" -A
